339789ff560e42eaf32697ff715eb61b6e64b7bb
namooTH <73419369+namooTH@users.noreply.github.com>
2023-10-06 19:47:48 +0700

    fixed + added some audio effect thing

--- internal/pausemenu/main.gd
+++ internal/pausemenu/main.gd
@@ -1,21 +1,20 @@
 extends Control
 
 var loadgame = false
+var notallowed = ["/root/Control", "/root/mapselector"]
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("ui_cancel"):
-		if visible == false:
-			get_tree().paused = true
-			visible = true
-		else:
-			get_tree().paused = false
-			visible = false
+		if not str(get_tree().get_current_scene().get_path()) in notallowed:
+			if visible == false:
+				AudioServer.set_bus_effect_enabled(0, 0, false)
+				get_tree().paused = true
+				visible = true
+			else:
+				AudioServer.set_bus_effect_enabled(0, 0, true)
+				get_tree().paused = false
+				visible = false
+		
 
 func toggleloadgame():
 	if loadgame == false:
