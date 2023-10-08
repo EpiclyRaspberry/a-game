eef434cae0caf18a11e9e25c6d54d62655a5139f
namooTH <73419369+namooTH@users.noreply.github.com>
2023-10-08 11:39:11 +0700

    place holding

--- internal/console/console.gd
+++ internal/console/console.gd
@@ -6,10 +6,12 @@ var mousein
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if OS.is_debug_build():
+		
+		#print(OS.get_static_memory_usage()/1000000)
 		if Input.is_action_just_pressed("toggleconsole"):
 			toggleconsolevisible()
 	if Input.is_action_just_pressed("mouseleft"):
-			click_pos = get_local_mouse_position()	
+		click_pos = get_local_mouse_position()	
 	if Input.is_action_pressed("mouseleft"):
 		position =(
 			Vector2(position) +	
