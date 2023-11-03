6ddd7c4fa74566f8c45973c6744c46d7ada62823
namooTH <73419369+namooTH@users.noreply.github.com>
2023-11-03 22:07:04 +0700

    improved things

--- library/lrc-parser/lrc-parser.gd
+++ library/lrc-parser/lrc-parser.gd
@@ -11,7 +11,7 @@ func parse(path):
 	# returns an array
 
 	
-func parse_get_only_timestamp_and_lyric(path):
+func parse_only_timestamp_and_lyric(path):
 	var parsed = []
 	for i in parse(path):
 		if i[0].replace(":", "").replace(".", "").is_valid_float():
@@ -19,3 +19,18 @@ func parse_get_only_timestamp_and_lyric(path):
 	return parsed
 
 	# same thing as parse function but removes all the unnessesary bloat from original lyric
+
+func parse_only_timestamp_and_lyric_minus_last_timestamp(path):
+	var counter = 0
+	var lasttime = 0
+	var parsed = []
+	var lyric = parse_only_timestamp_and_lyric(path)
+	
+	for i in lyric:
+		parsed.append([float(i[0].split(":")[0]) * 60 + float(i[0].split(":")[1]) - lasttime, lyric[counter][1] ])
+		lasttime = (float(i[0].split(":")[0]) * 60 + float(i[0].split(":")[1]))
+		counter += 1
+
+	return parsed
+	
+	# easier to code something like live lyric with
--- testing/lyrictest.gd
+++ testing/lyrictest.gd
@@ -1,22 +1,22 @@
 extends Label
 
-var timetodo = []
 var counter = 0
-var lyrics = Library.get_node("lrc_parser").parse_get_only_timestamp_and_lyric("res://assets/music/lyrics/heather-by-glaive-ericdoa.lrc")
-var lasttime = 0
+var lyrics = Library.get_node("lrc_parser").parse_only_timestamp_and_lyric_minus_last_timestamp("res://assets/music/lyrics/heather-by-glaive-ericdoa.lrc")
 
 func _ready():
-	for i in lyrics:
-		timetodo.append(float(i[0].split(":")[0]) * 60 + float(i[0].split(":")[1]) - lasttime)
-		lasttime = (float(i[0].split(":")[0]) * 60 + float(i[0].split(":")[1]))
 	$AudioStreamPlayer2D.play()
 
 func _on_timer_timeout():
-	if counter > 0:
-		text = lyrics[counter - 1][1]
-		$Timer.wait_time = timetodo[counter]
+	if counter < len(lyrics):
+		if counter > 0:
+			Notification.send("Lyric", lyrics[counter - 1][1], "normal")
+			text = lyrics[counter - 1][1]
+			$Timer.wait_time = lyrics[counter][0]
+		else:
+			$Timer.wait_time = lyrics[counter][0]
 		$Timer.start()
+		counter += 1
 	else:
-		$Timer.wait_time = timetodo[counter]
-		$Timer.start()
-	counter += 1
+		Notification.send("Lyric", lyrics[counter - 1][1], "normal")
+		text = lyrics[counter - 1][1]
+	
